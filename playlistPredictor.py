66f9ebd241344bd463cacc850c61a9a637af8587
Nick Carney <npcarney@uvm.edu>
2023-03-14 23:22:33 -0400

    working with playlist as input and creates new spotify playlist with predictions

--- playlistPredictor.py
+++ playlistPredictor.py
@@ -16,11 +16,9 @@ from scipy.spatial.distance import cdist
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 
-
-data = pd.read_csv( '/data.csv')
-genre_data = pd.read_csv( '/data_w_genres.csv')
-artist_data = pd.read_csv( '/data_by_artist.csv')
-
+data = pd.read_csv( 'data/data.csv')
+genre_data = pd.read_csv( 'data/data_w_genres.csv')
+artist_data = pd.read_csv( 'data/data_by_artist.csv')
 
 song_cluster_pipeline = Pipeline([('scaler', StandardScaler()), 
                                   ('kmeans', KMeans(n_clusters=20, 
@@ -33,8 +31,6 @@ song_cluster_pipeline.fit(X)
 song_cluster_labels = song_cluster_pipeline.predict(X)
 data['cluster_label'] = song_cluster_labels
 
-#!pip install spotipy
-
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from collections import defaultdict
@@ -44,9 +40,7 @@ from spotipy.oauth2 import SpotifyOAuth
 #export SPOTIPY_CLIENT_ID='ba503ee919b241a19afb3a14415d3095'
 #export SPOTIPY_CLIENT_SECRET='f628162109f840269d9bf8197f8f31cd'
 client_id="ba503ee919b241a19afb3a14415d3095"
-client_secret="secret"
-
-#fr#258cdcea1fd8494cb7a0105a282a3141
+client_secret = "54015e1aedf0406f960b102633ca6809"
 
 #sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id, client_secret))
@@ -74,7 +68,6 @@ def find_song(name, year):
         song_data[key] = value
 
     return pd.DataFrame(song_data)
-
 
 from collections import defaultdict
 from sklearn.metrics import euclidean_distances
@@ -127,7 +120,7 @@ def flatten_dict_list(dict_list):
 
 def recommend_songs( song_list, spotify_data, n_songs=12):
     
-    metadata_cols = ['name', 'year', 'artists']
+    metadata_cols = ['name', 'year', 'artists','id']
     song_dict = flatten_dict_list(song_list)
     
     song_center = get_mean_vector(song_list, spotify_data)
@@ -141,10 +134,9 @@ def recommend_songs( song_list, spotify_data, n_songs=12):
     rec_songs = rec_songs[~rec_songs['name'].isin(song_dict['name'])]
     return rec_songs[metadata_cols].to_dict(orient='records')
 
-
 import json
 import time
-from spotify_client_credentials import *
+#from spotify_client_credentials import *
 
 client_credentials_manager = SpotifyClientCredentials(client_id,client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
@@ -161,10 +153,11 @@ def get_track_ids(playlist_id):
 def get_track_data(track_id):
     meta = sp.track(track_id)
     track_details = {"name":meta['name'],"album":meta['album']['name'],"artist":meta['album']['artists'][0]['name'],
-                     "release_data":meta['album']['release_date'], "duration_in_mins":round((meta['duration_ms']*.001)/60,2)}
+                     "release_date":meta['album']['release_date'], "duration_in_mins":round((meta['duration_ms']*.001)/60,2)}
     return track_details
 
-playlist_id = '5796702459ca4d40'
+playlist_id = '42ukc3Z21ZLWO4F480YEbH'
+playlist_name = "''"+sp.playlist(playlist_id)['name']+"''"
 track_ids = get_track_ids(playlist_id)
 
 tracks = []
@@ -179,33 +172,48 @@ with open('spotify_data.json','w') as outfile:
 
 f = open('spotify_data.json')
 
-data = json.load(f)
+playlist_data = json.load(f)
 names = []
 years = []
 count = 0
 reccomender_str = ""
-for i in data:
-    names.append(i['name'])
-    years.append(i['release_date'][:4])
-    song_dict = dict(name=i['name'],year=i['release_date'][:4])
-    #print(song_dict)
-    #print(song_dict['name'])#, song_dict)
-    recommend_songs([{'name':str(song_dict['name']),'year':int(song_dict['year'])}],spotify_data)
-    
-    
-    #print(names[count],years[count])
-#     if(count!=len(track_ids)-1):
-#         reccomender_str+="{'name':'"+names[count]+"', 'year':"+years[count]+"},"
-#     else:
-#         reccomender_str+="{'name':'"+names[count]+"', 'year':"+years[count]+"}"
-    
-    count+=1
-#print(reccomender_str)   
-    
+dict_list = []
+for i in range(len(playlist_data)):
+    name = playlist_data[i]['name']
+    year = int(playlist_data[i]['release_date'].split('-')[0])
+    #print(i,name,year)
+    names.append(name)
+    years.append(year)
+    song_dict = dict(name=names[i],year=years[i])
+    dict_list.append(song_dict)
 f.close()
-# recommend_songs([reccomender_str],  data)
-# recommend_songs([{'name': 'Dream On', 'year':1980},
-#                 {'name': 'Hotel California - 2013 Remaster', 'year': 1976},
-#                 {'name': 'Break on Through (To the Other Side)', 'year': 1967},
-#                 {'name': 'Your Time Is Gonna Come - Remaster', 'year': 1969},
-#                 {'name': 'My Type', 'year': 2014}],  data)
+recommended_songs = recommend_songs(dict_list , data, len(playlist_data))
+     
+import spotipy.util as util
+user = '31t2mek3n4j35zwlcpdh77bgfce4'#my username
+
+#export SPOTIPY_CLIENT_ID='ba503ee919b241a19afb3a14415d3095'
+#export SPOTIPY_CLIENT_SECRET='258cdcea1fd8494cb7a0105a282a3141'
+
+scope = 'playlist-modify-public'
+token = util.prompt_for_user_token(user,
+                           scope,
+                           client_id='ba503ee919b241a19afb3a14415d3095',
+                           client_secret='54015e1aedf0406f960b102633ca6809',
+                           redirect_uri='https://example.com/callback')
+sp2 = spotipy.Spotify(auth=token)
+sp2.trace = False
+new_playlist = sp2.user_playlist_create(user, playlist_name+" PREDICTED", public=True, description='made based on machine learning predictions using the playlist'+playlist_name)
+
+songs_to_add = []
+for i in range(len(recommended_songs)):
+    songs_to_add.append(recommended_songs[i]['id'])
+
+repeat = False
+for i in range(len(recommended_songs)):
+    for i in range(len(sp.playlist(new_playlist['id'])['tracks']['items'])):
+        if(sp.playlist(new_playlist['id'])['tracks']['items'][i]['track']['id']==recommended_songs[i]['id']):
+            repeat=True
+    if(repeat == False):
+        results = sp2.user_playlist_add_tracks(user, new_playlist['id'], songs_to_add)
+
